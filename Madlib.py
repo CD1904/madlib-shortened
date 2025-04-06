7bd50de995ab3209f2cb723bdc0d5c6630ecf580
CD1904 <cduffyhayes19@gmail.com>
2025-04-06 02:14:54 +0100

    Create Madlib.py

---
+++ Madlib.py
@@ -0,0 +1,6 @@
+adjective1, noun, adjective2, verb, adjective3 = input("Enter an adjective, noun, adjective, verb, adjective: ").split()
+
+print(f"Today I went to the {adjective1} zoo.")
+print(f"In an exhibit, I saw a {noun}.")
+print(f"The {noun} was {adjective2} and {verb}ing.")
+print(f"I was {adjective3}.")
